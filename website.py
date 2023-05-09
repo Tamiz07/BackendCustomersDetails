0cbeaa431857da87cefe545a4b634bf728af8908
Tamiz07 <118349184+Tamiz07@users.noreply.github.com>
2023-05-09 11:50:39 +0530

    Update website.py

--- website.py
+++ website.py
@@ -6,6 +6,17 @@ class Website:
     def __init__(self):
         self.customer = Customer()
         self.product = Products
+        
+    # web page design
+    def web_design(self):
+        # This is the header portion of the design
+        header = "Welcome to ShopKART!"
+        print(header.center(80))
+        param = "India's no.1 online shopping..!"
+        print(param.center(80))
+        reg = "New to our shopKART kindly Signup"
+        print()
+        print(reg)
 
     def signup(self):
         return self.customer.signup()
@@ -18,6 +29,7 @@ class Website:
 
 
 web = Website()
+web.web_design()
 web.signup()
 web.login()
 web.show_product()
